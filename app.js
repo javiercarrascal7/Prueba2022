9439cb2af830761c7aa0ea09ade794c1d0743cc3
javiercarrascal7 <franciscojavier.carrascal.perez@acciona.com>
2025-12-10 12:09:06 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+document.getElementById("main-title").innerText =
+  document.getElementById("main-title").innerText + ", ¿Como estas?";
+
+//document.body.innerHTML ="<h1>Time right now is: </h1>"//escribe todo el body y borra todo lo anterior
+ 
+/*let d = new Date();
+document.body.innerHTML =
+"<h1>Time right now is:  " +
+d.getHours() +
+":" +
+d.getMinutes() +
+":" +
+d.getSeconds();
+("</h1>");*/
+
+console.log("Hola a todos");
+var msg = "Hello World";
+console.log(msg);
